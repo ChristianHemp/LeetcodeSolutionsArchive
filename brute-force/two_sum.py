e7a9d5f69bb96c288c3b65f05cd018bb7057cd04
Christian Hemprich <hemprich.c@gmail.com>
2025-09-15 20:45:26 -0700

    Create two_sum.py

---
+++ brute-force/two_sum.py
@@ -0,0 +1,13 @@
+# Problem: https://leetcode.com/problems/plus-one/description/
+# Approach: Nested for loop to brute force the solution
+# Complexity: O(n²) time, O(1) space
+
+# Note: Inteded to be solved with hash map, completed in first few hours of learning programming so solved brute force
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for x in range(n):
+            for y in range(x + 1, n):
+                if nums[x] + nums[y] == target:
+                    return[x, y]
