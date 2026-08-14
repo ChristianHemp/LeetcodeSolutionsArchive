3efd81a1f7353da3d65f59385378a02a2c0c991e
Christian Hemprich <hemprich.c@gmail.com>
2026-08-14 14:26:40 -0700

    Update 1700-Number-of-Students-Unable-to-Eat-Lunch.py

--- queue/1700-Number-of-Students-Unable-to-Eat-Lunch.py
+++ queue/1700-Number-of-Students-Unable-to-Eat-Lunch.py
@@ -1,6 +1,6 @@
 # Problem: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
 # Approach: Keep a counter of the number of repititions to break out of loop, if beginning of queues are same remove from both queues and reset reps, otherwise add the student to end of the queue and increment reps
-# Complexity: O(n^2) time, O(n) space. (optimal despite n^2)
+# Complexity: O(n^2) time, O(n) space.
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
