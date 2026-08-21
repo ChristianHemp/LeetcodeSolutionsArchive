e87a0748865a9d087d91e4460f267847c6782b28
Christian Hemprich <hemprich.c@gmail.com>
2026-08-21 15:51:54 -0700

    Add shortest path algorithm for binary matrix
    Implemented a breadth-first search to find the shortest path in a binary matrix. The function returns the length of the path or -1 if no path exists.

---
+++ arrays/1091-Shortest-Path-in-a-Binary-Matrix.py
@@ -0,0 +1,41 @@
+# Problem: https://leetcode.com/problems/shortest-path-in-binary-matrix/
+# Approach: Perform a breadth first search starting from (0, 0) checking all possible path directions and incrementing length. return length when end is reached
+# Complexity: O(n * m) time where n is rows and m is cols, O(n * m) space
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        q = deque()
+        visited = set()
+        length = 1
+
+        q.append((0, 0))
+        visited.add((0, 0))
+
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+
+                if r == rows - 1 and c == cols - 1:
+                    return length
+                
+                neighbor_directions = ((1, 0), (-1, 0), (0, 1), (0, -1),
+                                       (1, 1), (-1, 1), (1, -1), (-1, -1))
+                
+                for dr, dc in neighbor_directions:
+                    nr = r + dr
+                    nc = c + dc
+                    
+                    if (nr < 0 or nc < 0 or nr >= rows or nc >= cols or
+                        (nr, nc) in visited or grid[nr][nc] == 1):
+                        continue
+                    
+                    q.append((r + dr, c + dc))
+                    visited.add((r + dr, c + dc))
+            length += 1
+        
+        return -1
