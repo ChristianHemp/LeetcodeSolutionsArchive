39422c3cead2b9b82e486484c9f318bf08c75852
Christian Hemprich <hemprich.c@gmail.com>
2026-08-20 23:26:08 -0700

    Add lengthOfLastWord function in Solution class
    Implement function to calculate length of last word.

---
+++ strings/58-Length-of-Last-Word.py
@@ -0,0 +1,9 @@
+# Problem: https://leetcode.com/problems/length-of-last-word/
+# Approach: Strip whitespace from ends of s, split words into list and return length of last element in list
+# Complexity: O(n) time, O(n) space
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.strip().split()
+
+        return len(words[-1])
