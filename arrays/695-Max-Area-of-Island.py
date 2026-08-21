5606b5854cf3330ffc87a2a682639dd31a686af3
Christian Hemprich <hemprich.c@gmail.com>
2026-08-21 14:58:52 -0700

    Add maxAreaOfIsland function for island area calculation
    Implement maxAreaOfIsland function to calculate the largest area of an island in a grid.

---
+++ arrays/695-Max-Area-of-Island.py
@@ -0,0 +1,33 @@
+# Problem: https://leetcode.com/problems/max-area-of-island/
+# Approach: Search through matrix indices for island indices and remove all adjacent island indicies while incrementing curr_size. At each island update result if the new island size larger than current largest (res).
+# Complexity: O(n * m) time where n is rows and m is cols, O(n * m) space (callstack)
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        curr_size = 0
+        res = 0
+
+        def dfs(r, c):
+            nonlocal curr_size
+
+            if (r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or
+                grid[r][c] == 0):
+                return
+            
+            grid[r][c] = 0
+            curr_size += 1
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+                    res = max(res, curr_size)
+                    curr_size = 0
+        
+        return res
+
