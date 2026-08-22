cdc40dc8eb77d93135a0e54644a16ef1c0967101
Christian Hemprich <hemprich.c@gmail.com>
2026-08-22 16:52:26 -0700

    Refactor uniquePathsWithObstacles for obstacle handling
    Updated the uniquePathsWithObstacles method to handle obstacles in the grid more effectively.

--- dynamic-programming/63-Unique-Paths-II.py
+++ dynamic-programming/63-Unique-Paths-II.py
@@ -2,6 +2,7 @@
 # Approach: Built upon submission for LC 62 (see /dynamic-programming/62-Unique-Paths.py). Changes: Instead of always having last index in each row be 1, it mirrors value seen in the last index of previous row.
 #           This helps cover cases where there is a rock somewhere in the last column. Additionally, curr_row is only updated with the sum of right and down if it isn't a rock in the given grid, otherwise it is set to 0, as there are no paths from there to the end.
 # Complexity: O(r * c) time, O(c) space where r is num of rows and c is num of cols
+# Enjoyment: 4/5
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
