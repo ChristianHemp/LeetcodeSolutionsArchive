650e06ae364228ee1ba9a099bfed72e364f600fe
Christian Hemprich <hemprich.c@gmail.com>
2026-08-22 15:41:32 -0700

    Improve comments and dp logic in House Robber solution
    Refactor comments for clarity and adjust dp initialization logic. Simplify the decision-making process for robbing or skipping houses.

--- dynamic-programming/198-House-Robber.py
+++ dynamic-programming/198-House-Robber.py
@@ -1,8 +1,6 @@
 # Problem: https://leetcode.com/problems/house-robber/
-# Approach: Use dp array to keep track of max amount of money to have made at that index. For each iteration while i < length of array, set dp[i] to the sum of nums[i] to dp[i - 2].
-#           This is the default case where we choose to rob the current house available. The second case is to not rob the house, however, to account for double skips (ie: 2, 1, 1, 2),
-#           if dp[i - 2] is greater than dp[i - 1], move the value over one so that dp[i - 1] still accurately reflects max amount possible at that time having not robbed the current house.
-# Complexity: O(n) time, O(n) space
+# Approach: Use dp array to keep track of max amount of money to have made at that index. At each new house we can choose to rob or skip, and dp[i] will keep the larger of the two values.
+# Complexity: O(n) time, O(n) space (could optimize to O(1) space using two pointers, but dp logic clearer this way)
 # Enjoyment: 5/5
 
 class Solution:
@@ -14,20 +12,15 @@ class Solution:
         elif length <= 2:
             return max(nums[0], nums[1])
         
-        # initialize dp array
         dp = [0] * length
-        dp[0], dp[1] = nums[0], nums[1]
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
 
         i = 2
-        # 2 choices: 1 - rob house, 2 - skip house
         while i < length:
-            # default case 1
-            dp[i] = dp[i - 2] + nums[i]
-            
-            # maintains order for choice 2 (dp[i-1] must be max possible money gained having not robbed previous house)
-            if dp[i - 2] > dp[i - 1]:
-                dp[i - 1] = dp[i - 2]
-    
+            # choice 1: rob house i
+            # choice 2: skip house i
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
             i += 1
 
-        return max(dp[-1], dp[-2])
+        return dp[-1]
