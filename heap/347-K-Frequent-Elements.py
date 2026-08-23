a2da3a3393f458a41a3d884bc7b90425f7469e1b
Christian Hemprich <hemprich.c@gmail.com>
2026-08-23 12:57:50 -0700

    Add topKFrequent function for k frequent elements
    Implement topKFrequent function to find the k most frequent elements using a heap.

---
+++ heap/347-K-Frequent-Elements.py
@@ -0,0 +1,22 @@
+# Problem: https://leetcode.com/problems/top-k-frequent-elements/
+# Approach: Use a hashmap to keep counts of values, add a tuple containing the frequency its element to a heap that sorts based on frequency. Pop from that heap k times adding the element to res.
+# Complexity: O(n) time, O(n) space
+
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = defaultdict(int)
+        pq = []
+        res = []
+
+        for num in nums:
+            counts[num] += 1
+        
+        for key, val in counts.items():
+            heapq.heappush_max(pq, (val, key))
+        
+        for _ in range(k):
+            res.append(heapq.heappop_max(pq)[1])
+        
+        return res
