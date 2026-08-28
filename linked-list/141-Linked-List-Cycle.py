604429f486d5225e83ddc99021efabe4a535f41b
Christian Hemprich <hemprich.c@gmail.com>
2026-08-27 19:57:35 -0700

    Update 141-Linked-List-Cycle.py

--- linked-list/141-Linked-List-Cycle.py
+++ linked-list/141-Linked-List-Cycle.py
@@ -1,15 +1,22 @@
 # Problem: https://leetcode.com/problems/linked-list-cycle/
-# Approach: Keep hashmap of seen nodes and cycle through linkedlist looking for dupliactes
-# Complexity: O(n) time, O(n) space
+# Approach: Use two pointers, one that moves 2 nodes at a time one that moves 1 node at a time. With no cycles, the fast node will reach end and return False, if it ever becomes equal to slow, return True.
+# Complexity: O(n) time, O(1) space
+# Enjoyment: 3/5
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
+        if head is None:
+            return False
         
-        while head != None:
-            if head in seen:
-                return True
-            seen.add(head)
-            head = head.next
-        
-        return False
+        fast = head.next
+        slow = head
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            fast = fast.next.next
+            slow = slow.next
+
+        return True
+
+
