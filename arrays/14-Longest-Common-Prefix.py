fb3b35b7f2a90a51926a54f359d3fea485870e14
Christian Hemprich <hemprich.c@gmail.com>
2026-08-29 16:44:13 -0700

    Update 14-Longest-Common-Prefix.py

--- arrays/14-Longest-Common-Prefix.py
+++ arrays/14-Longest-Common-Prefix.py
@@ -1,18 +1,21 @@
 # Problem: https://leetcode.com/problems/longest-common-prefix/
-# Approach: Only look at smallest and largest strings, compare each character adding to lcp until no longer equivalent, then return.
-# Complexity: O(n) time, O(1) space
+# Approach: Only look at smallest and largest strings, compare each character adding to chars then return the join of chars
+# Complexity: O(n) time, O(n) space
+# Enjoyment: 3/5
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        lcp = ''
-        largest = max(strs)
-        smallest = min(strs)
         if len(strs) == 0:
-            return lcp
-        for i in range(len(smallest)):
-            if smallest[i] == largest[i]:
-                lcp += smallest[i]
+            return ''
+
+        longest = max(strs)
+        shortest = min(strs)
+        chars = []
+
+        for i in range(len(shortest)):
+            if shortest[i] == longest[i]:
+                chars.append(shortest[i])
             else:
                 break
-
-        return lcp
+        
+        return ''.join(chars)
