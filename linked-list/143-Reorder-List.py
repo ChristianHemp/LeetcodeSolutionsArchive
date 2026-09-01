e15b9f19cd364206c182727687b118f77350bbaf
Christian Hemprich <hemprich.c@gmail.com>
2026-09-01 11:18:50 -0700

    Update 143-Reorder-List.py

--- linked-list/143-Reorder-List.py
+++ linked-list/143-Reorder-List.py
@@ -1,35 +1,37 @@
 # Problem: https://leetcode.com/problems/reorder-list/
-# Approach: Find middle of list using fast and slow pointers. Split list in half. Reverse second half. Merge first half with newly reversed second half.
+# Approach: Find middle index, reverse second half of linked list from middle index, alternate between both lists and merge
 # Complexity: O(n) time, O(1) space
-# Enjoyment: 2/5
+# Enjoyment: 3/5
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        right = head
         mid = head
+        right = head
 
+        # find middle index
         while right.next and right.next.next:
-            right = right.next.next
             mid = mid.next
+            right = right.next.next
         
-        second = mid.next
+        temp = mid.next
         mid.next = None
-
+        second = temp
         prev = None
-        curr = second
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+
+        # reverse second half of linked list
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
         
-        # reversed head at prev
+        # alternate and merge both lists
         while prev:
             temp = head.next
             temp2 = prev.next
 
             head.next = prev
             prev.next = temp
-
+            
             head = temp
             prev = temp2
