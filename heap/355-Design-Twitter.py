90ca0cac8f0501a594b5b54737e567d143057bb1
Christian Hemprich <hemprich.c@gmail.com>
2026-09-08 19:05:01 -0700

    Create 355-Design-Twitter.py

---
+++ heap/355-Design-Twitter.py
@@ -0,0 +1,51 @@
+# Problem: https://leetcode.com/problems/design-twitter/
+# Approach: Use hashmaps to keep track of user tweets and followees. Take most recent tweet from self and all followees and put into heap to get most recent post cycling until heap is empty or 10 posts are found.
+# Complexity: O(n log n) getNewsFeed, O(1) rest, O(n) space
+# Enjoyment: 3/5
+
+from collections import defaultdict
+
+class Twitter:
+    def __init__(self):
+        self.time = 0
+        self.tweets = defaultdict(list)
+        self.following = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        # negative time for max heap
+        self.tweets[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        possible_tweets = []
+        feed = []
+
+        users = set(self.following[userId])
+        users.add(userId)
+
+        for user in users:
+            if self.tweets[user]:
+                index = len(self.tweets[user]) - 1
+                time, tweetId = self.tweets[user][index]
+                possible_tweets.append((-time, tweetId, user, index))
+        
+        heapq.heapify(possible_tweets)
+
+        while possible_tweets and len(feed) < 10:
+            _, tweetId, user, index = heapq.heappop(possible_tweets)
+            feed.append(tweetId)
+
+            if index > 0:
+                next_index = index - 1
+                time, tweetId = self.tweets[user][next_index]
+                heapq.heappush(possible_tweets, (-time, tweetId, user, next_index))
+        
+        return feed
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
